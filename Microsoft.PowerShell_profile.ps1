963caf2fa175b3b92957aae7e1d981a0e15490e3
Joe Abou Serhal <joeabouserhal77777@gmail.com>
2022-09-29 18:26:14 +0300

    reordered files

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,8 +0,0 @@
-$mojada = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\mojada.omp.json"
-$1_shell = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\1_shell.omp.json"
-$dracula = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\dracula.omp.json"
-$dracula_custom = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\dracula_custom.omp.json"
-$default = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\default.omp.json"
-$desktop = "C:\Users\Joe\Desktop"
-
-oh-my-posh init pwsh --config $dracula_custom| Invoke-Expression
