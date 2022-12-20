4fb0b0d48b1c901d1b43aa264fc799d8556253a2
Joe Abou Serhal <80884927+joeabouserhal@users.noreply.github.com>
2022-12-20 19:27:01 +0200

    added custom theme and tere config

--- powershell/Microsoft.PowerShell_profile.ps1
+++ powershell/Microsoft.PowerShell_profile.ps1
@@ -3,6 +3,16 @@ $1_shell = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\1_shell.omp.js
 $dracula = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\dracula.omp.json"
 $dracula_custom = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\dracula_custom.omp.json"
 $default = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\default.omp.json"
+$tiwahu = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\tiwahu.omp.json"
+$tiwahu_custom = "C:\Users\Joe\AppData\Local\Programs\oh-my-posh\themes\tiwahu_custom.omp.json"
 $desktop = "C:\Users\Joe\Desktop"
 
-oh-my-posh init pwsh --config $dracula_custom| Invoke-Expression
+oh-my-posh init pwsh --config $tiwahu_custom| Invoke-Expression
+
+function Invoke-Tere() {
+    $result = . (Get-Command -CommandType Application tere) $args
+    if ($result) {
+        Set-Location $result
+    }
+}
+Set-Alias tere Invoke-Tere
